c1777b61668001103fe66aa1f97864bfbd9a00c4
mboncheva <m_boncheva@abv.bg>
2018-05-24 20:09:22 +0300

    C# Web Development Basics Labs and Exercises

---
+++ C# Web Devolpment Basic-May2018/WEB SERVER HTTP PROTOCОL/URLCode/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLCode
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var url = Console.ReadLine();
+            var decodeUrl = WebUtility.UrlDecode(url);
+            Console.WriteLine(decodeUrl);
+        }
+    }
+}
