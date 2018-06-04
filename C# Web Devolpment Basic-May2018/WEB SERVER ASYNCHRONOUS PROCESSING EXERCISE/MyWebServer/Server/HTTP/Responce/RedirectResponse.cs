5f71af0ae0caa57797818c41fec69fe83d6b6ae4
mboncheva <m_boncheva@abv.bg>
2018-06-04 17:36:39 +0300

    C# Web Development Basics Labs and Exercises

---
+++ C# Web Devolpment Basic-May2018/WEB SERVER ASYNCHRONOUS PROCESSING EXERCISE/MyWebServer/Server/HTTP/Responce/RedirectResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace MyWebServer.Server.HTTP.Responce
+{
+    using Common;
+    using Enums;
+
+    public class RedirectResponse : HttpResponse
+    {
+        public RedirectResponse(string redirectUrl)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(redirectUrl, nameof(redirectUrl));
+
+            this.StatusCode = HttpStatusCode.Found;
+            this.Headers.Add(new HttpHeader("Location", redirectUrl));
+            this.Headers.Add(HttpHeader.Location, redirectUrl);
+        }
+
+    }
+}
---
+++ C# Web Devolpment Basic-May2018/WEB SERVER ASYNCHRONOUS PROCESSING EXERCISE/MyWebServer/Server/Routing/RoutingContext.cs
@@ -0,0 +1,24 @@
+﻿namespace MyWebServer.Server.Routing
+{
+    using System.Collections.Generic;
+    using Contracts;
+    using Handlers;
+    using Common;
+
+    public class RoutingContext : IRoutingContext
+    {
+
+        public RoutingContext(RequestHandler handler, IEnumerable<string> parameters)
+        {
+            CoreValidator.ThrowIfNull(handler, nameof(handler));
+            CoreValidator.ThrowIfNull(parameters, nameof(parameters));
+
+            this.Handler = handler;
+            this.Parameters = parameters;
+        }
+
+        public IEnumerable<string> Parameters { get; private set; }
+
+        public RequestHandler Handler { get; private set; }
+    }
+}
