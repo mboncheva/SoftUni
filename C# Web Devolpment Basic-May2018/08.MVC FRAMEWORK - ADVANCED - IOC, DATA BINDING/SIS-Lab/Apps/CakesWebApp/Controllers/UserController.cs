9b70f522a2e0422c331b4070a6ea68ce3a94e479
mboncheva <m_boncheva@abv.bg>
2018-11-30 15:51:50 +0200

    Implemented cakes profile page

---
+++ C# Web Devolpment Basic-May2018/08.MVC FRAMEWORK - ADVANCED - IOC, DATA BINDING/SIS-Lab/Apps/CakesWebApp/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿namespace CakesWebApp.Controllers
+{
+    using CakesWebApp.ViewModels.User;
+    using SIS.HTTP.Responses;
+    using SIS.MvcFramework;
+    using System.Linq;
+
+    public class UserController : BaseController
+    {
+        [HttpGet("/user/profile")]
+        public IHttpResponse Profile()
+        {
+            var viewModel = this.Db.Users.Where(x => x.Username == this.User)
+                .Select(x => new ProfileViewModel
+                {
+                    Username = x.Username,
+                    RegisterOn = x.DateOfRegistration,
+                    OrdersCount = x.Orders.Count()
+                })
+                .FirstOrDefault();
+
+            if (viewModel == null)
+            {
+                return this.BadRequestError("Profile page not accessible for this user.");
+            }
+
+            return this.View("Profile", viewModel);
+        }
+    }
+}
